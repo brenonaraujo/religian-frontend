00dbf434cd18827bf9773cca76579b362a1bafa1
Brenon Araujo <brenonaraujo@gmail.com>
2019-10-27 20:18:01 -0300

    changed the api host

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL : 'https://3.95.152.68:1337'});
+const api = axios.create({baseURL : 'https://religian-api.herokuapp.com'});
 
 export default api;
